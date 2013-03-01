db8694769e38089fbf1b3dfe3bf8b54c3e72df78
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-03-02 08:43:01 +1100

    Fleshed out User model.

--- src/org/the86/model/User.java
+++ src/org/the86/model/User.java
@@ -1,20 +1,56 @@
 package org.the86.model;
 
+import java.util.Date;
+
 public class User extends The86Object {
-
-	private String avatar_url;
-
-	private String email;
-
-	private String location;
 
 	private String name;
 
-	private String profile;
+	private String email;
+
+	private String avatar_url;
 
 	private String twitter_username;
 
+	private String profile;
+
+	private String location;
+
+	private Date created_at;
+
 	private String url;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getAvatarUrl() {
+		return avatar_url;
+	}
+
+	public String getTwitterUsername() {
+		return twitter_username;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public Date getCreatedAt() {
+		return created_at;
+	}
+
+	public String getUrl() {
+		return url;
+	}
 
 	public String toString() {
 		return String.format("%s <%s>", name, email);
